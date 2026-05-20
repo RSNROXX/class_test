55d824aba580cf1869e6b5911dd165a14a35578d
Rishikesh Sanjay Naware <rishikeshnaware7@gmail.com>
2026-05-20 13:30:56 +0200

    Making the main.py file productions ready

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+"""
+This module contains basic mathematical operations.
+Functions for multiplying and adding two numbers.
+"""
+
+
 def prod_number(num1, num2):
+    '''Return the product of 2 numbers'''
     return num1 * num2
 
+
 def add_number(num1, num2):
+    '''Return the addition of 2 numbers'''
     return num1 + num2
 
-print(prod_number(3 , 4))
-print(add_number(1 , 2))
+
+print(prod_number(3, 4))
+print(add_number(1, 2))
