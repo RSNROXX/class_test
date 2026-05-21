fa0fa6eb51d709cfd421262b8867ae16cdd22bc3
Rishikesh Sanjay Naware <rishikeshnaware7@gmail.com>
2026-05-21 14:13:57 +0200

    Docs: Adding proper Docstring for all functions in main.py

--- main.py
+++ main.py
@@ -12,24 +12,48 @@ X_INIT: int = 1
 
 def prod_number(num1: int,
                 num2: int) -> int:   # type hints
-    """Return the product of 2 numbers"""
+    """Func to multiply numbers
+
+    Args:
+        num1 (int): _description_
+        num2 (int): _description_
+
+    Returns:
+        int: _description_
+    """
     return num1 * num2
 
 
 def add_number(num1: int,
                num2: int) -> int:    # type hints
-    """Return the addition of 2 numbers"""
+    """Func to add Numbers
+
+    Args:
+        num1 (int): _description_
+        num2 (int): _description_
+
+    Returns:
+        int: _description_
+    """
     return num1 + num2
 
 
 def divide_number(num1: int,
                   num2: int) -> float:    # type hints
-    """Return the division of 2 numbers"""
+    """Func to Divide numbers
+
+    Args:
+        num1 (int): _description_
+        num2 (int): _description_
+
+    Returns:
+        float: _description_
+    """
     return num1 / num2
 
 
 def addition_example(old_inventory: int, new_inventory: int) -> int:
-    """_summary_
+    """This is the main example that was discussed heavy, for almost 2 lecs.
 
     Args:
         old_inventory (int): _description_
